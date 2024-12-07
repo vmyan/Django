c2704b84f6e41f1a540855f67dba499bc964767b
nyamakin <52594806+vmyan@users.noreply.github.com>
2024-12-07 18:24:29 +0300

    orm_migrations

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    object_list = Student.objects.all().order_by('group').prefetch_related('teachers')
+    context = {
+        'object_list': object_list
+    }
+    return render(request, template, context)
